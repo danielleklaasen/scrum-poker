000c6fd6d75ac1d268b47c9b47e6aef165caf6b4
Danielle Klaasen <danielle@gravity.nl>
2020-03-05 18:12:34 +0100

    Remove unnecessary content

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,18 +2,15 @@ import React from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
+import BackgroundSection from "../components/background-image"
 
 const IndexPage = () => (
   <Layout>
+    <BackgroundSection/>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
+    <h1>Gravity</h1>
+    <p>Scrum cards</p>
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
