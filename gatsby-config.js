8ea16a2c244079e737436170287f3eef950810ac
Danielle Klaasen <danielle@gravity.nl>
2020-03-05 18:11:18 +0100

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Gravity Scrum`,
+    description: `Scrum poker cards`,
+    author: `@danielleklaasen`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
